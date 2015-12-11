0b25f14ecffb7d4e2ea50f6e16dfa7480622cb04
michaeldegroot <specamps@gmail.com>
2015-12-11 15:47:47 +0100

    Only .search is now accessible from outside the module; scrapImage function now returns a array with 20 results

--- app.js
+++ app.js
@@ -5,46 +5,48 @@ var fs = require('fs');
 var path = require('path');
 var http = require('http');
 
-exports.getResponse = function(search,cb){
-	var url = 'http://www.google.com/search?q='+search+'&safe=off&source=lnms&tbm=isch&sa=X'
-	
-	request(url, function (err, response, body) {
+exports.search = function(data,cb){
+	if(!data.search) return(cb("Please define what you want to search",false));
+	if(!cb) throw new Error("No callback defined");
+	getResponse(data.search,function(err,response){
+		if(!response) return(cb(err,false));
+		scrapImage(response.body,function(err,imgs){
+			if(!imgs) return(cb(err,false));
+			if(data.save){
+				saveImage({img:imgs[0],save:data.save},function(err,filename){
+					if(!filename) return(cb(err,false));
+					return(cb(false,filename));
+				});
+			}
+			if(!data.save) cb(false,imgs); 
+		})
+	});
+}
+
+function getResponse(search,cb){
+	request('http://www.google.com/search?q='+search+'&safe=off&source=lnms&tbm=isch&sa=X', function (err, response, body) {
 		if (err) return(cb(err,false))
 		if(response.statusCode != 200) return(cb("HTTP code is not giving a 200. Cannot continue, current HTTP code: "+response.statusCode),false);
 		cb(false, {response:response,body:body});
 	});
 }
 
-exports.scrapImage = function(body,cb){
+function scrapImage(body,cb){
 	var $ = cheerio.load(body);
 	var table = $('table[class=images_table]').html();
 	if(!table) return(cb("Table class='images_table' could not be found in the scrapped results!",false));
-	var firstImage = $('table[class=images_table] tr td a img').first()[0].attribs.src;
-	cb(false,firstImage);
+	var arrayImage = $('table[class=images_table] tr td a img');
+	var images = [];
+	for(i=0;i<arrayImage.length;i++){
+		images.push(arrayImage[i].attribs.src);
+	}
+	cb(false,images);
 }
 
-exports.saveImage = function(data,cb){
+function saveImage(data,cb){
 	var filename = data.img.split("/")[data.img.split("/").length-1].split("images?q=tbn:")[1] + ".png";
 	request.get(data.img).on('error', function(err) {
 		return(cb(err,false));
 	}).pipe(fs.createWriteStream(path.join(data.save,filename)))
 	cb(false,filename);
-}
-
-exports.search = function(data,cb){
-	if(!data.search) return(cb("Please define what you want to search",false));
-	if(!cb) throw new Error("No callback defined");
-	exports.getResponse(data.search,function(err,response){
-		if(!response) return(cb(err,false));
-		exports.scrapImage(response.body,function(err,img){
-			if(!img) return(cb(err,false));
-			if(data.save){
-				exports.saveImage({img:img,save:data.save},function(err,filename){
-					if(!filename) return(cb(err,false));
-					return(cb(false,filename));
-				});
-			}
-			if(!data.save) cb(false,img); 
-		})
-	});
 }
--- test/test.js
+++ test/test.js
@@ -3,39 +3,18 @@ var assert = require('assert-plus');
 var fs = require('fs');
 var path = require('path');
 var googleImageSearch = require('../app');
-var html;
-
-describe("Internal Functions", function(){
-	this.timeout(5500);
-	it('.getResponse()', function(done){
-		googleImageSearch.getResponse("test",function(err,data){
-			assert.equal(data.response.statusCode,200);
-			html = data.body;
-			done();
-		});
-	});
-	it(".scrapImage()", function(done){
-		googleImageSearch.scrapImage(html,function(err,data){
-			assert.equal(err,false);
-			html = data;
-			done();
-		});
-	});
-});
 
 describe("API", function(){ 
 	this.timeout(5500);
 	it(".search", function(done){
 		googleImageSearch.search({search:"test"},function(err,data){
 			assert.equal(err,false);
-			html = data
 			done();
 		});
 	});
 	it(".search and save to file", function(done){
 		googleImageSearch.search({search:"test",save:"C:/Users/CatsPC/Desktop/NPM/google-image-search/test"},function(err,data){
-			assert.equal(data,"ANd9GcSXNJw-kfJrMV6drb80jGapDlCC_wcuniW_FUlX-q3WkBgdUGAyWuVvzkM.png");
-			html = data
+			assert.equal(err,false);
 			done();
 		});
 	});
