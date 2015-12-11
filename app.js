0d9c0d2ffadaf715adb642174830ddaa1e44386d
michaeldegroot <specamps@gmail.com>
2015-12-11 16:05:45 +0100

    NPM async code style guide

--- app.js
+++ app.js
@@ -9,25 +9,25 @@ exports.search = function(data,cb){
 	if(!data.search) return(cb("Please define what you want to search",false));
 	if(!cb) throw new Error("No callback defined");
 	getResponse(data.search,function(err,response){
-		if(!response) return(cb(err,false));
+		if(!response) return(cb(err,null));
 		scrapImage(response.body,function(err,imgs){
-			if(!imgs) return(cb(err,false));
+			if(!imgs) return(cb(err,null));
 			if(data.save){
 				saveImage({img:imgs[0],save:data.save},function(err,filename){
-					if(!filename) return(cb(err,false));
-					return(cb(false,filename));
+					if(!filename) return(cb(err,null));
+					return(cb(null,filename));
 				});
 			}
-			if(!data.save) cb(false,imgs); 
+			if(!data.save) cb(null,imgs); 
 		})
 	});
 }
 
 function getResponse(search,cb){
 	request('http://www.google.com/search?q='+search+'&safe=off&source=lnms&tbm=isch&sa=X', function (err, response, body) {
-		if (err) return(cb(err,false))
+		if (err) return(cb(err,null))
 		if(response.statusCode != 200) return(cb("HTTP code is not giving a 200. Cannot continue, current HTTP code: "+response.statusCode),false);
-		cb(false, {response:response,body:body});
+		cb(null, {response:response,body:body});
 	});
 }
 
@@ -40,13 +40,13 @@ function scrapImage(body,cb){
 	for(i=0;i<arrayImage.length;i++){
 		images.push(arrayImage[i].attribs.src);
 	}
-	cb(false,images);
+	cb(null,images);
 }
 
 function saveImage(data,cb){
 	var filename = data.img.split("/")[data.img.split("/").length-1].split("images?q=tbn:")[1] + ".png";
 	request.get(data.img).on('error', function(err) {
-		return(cb(err,false));
+		return(cb(err,null));
 	}).pipe(fs.createWriteStream(path.join(data.save,filename)))
-	cb(false,filename);
+	cb(null,filename);
 }
--- test/test.js
+++ test/test.js
@@ -8,13 +8,13 @@ describe("API", function(){
 	this.timeout(5500);
 	it(".search", function(done){
 		googleImageSearch.search({search:"test"},function(err,data){
-			assert.equal(err,false);
+			assert.equal(err,null);
 			done();
 		});
 	});
 	it(".search and save to file", function(done){
 		googleImageSearch.search({search:"test",save:"C:/Users/CatsPC/Desktop/NPM/google-image-search/test"},function(err,data){
-			assert.equal(err,false);
+			assert.equal(err,null);
 			done();
 		});
 	});
