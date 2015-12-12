e7e5d03704f4cff42181b1c0a57e3790dd62df20
michaeldegroot <specamps@gmail.com>
2015-12-12 12:11:07 +0100

    can now use pixplorer api; better error handling on pipe; ability to get small, medium or large images

--- app.js
+++ app.js
@@ -1,51 +1,100 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var fs = require('fs');
-var path = require('path');
-var http = require('http');
+'use strict'
+
+var request = require('request')
+var cheerio = require('cheerio')
+var fs = require('fs')
+var path = require('path')
+var http = require('http')
+var sha256 = require('sha256');
+var options = {api:"pixplorer"}
+
+exports.scraper = function(sets){
+  if(sets.api){
+    if(sets.api != "pixplorer" && sets.api != "google") throw new Error("Unknown api: "+sets.api);
+  }
+  options = sets
+}
 
 exports.search = function(data,cb){
-	if(!data.search) return(cb("Please define what you want to search",false));
-	if(!cb) throw new Error("No callback defined");
-	getResponse(data.search,function(err,response){
-		if(!response) return(cb(err,null));
-		scrapImage(response.body,function(err,imgs){
-			if(!imgs) return(cb(err,null));
-			if(data.save){
-				saveImage({img:imgs[0],save:data.save},function(err,filename){
-					if(!filename) return(cb(err,null));
-					return(cb(null,filename));
-				});
-			}
-			if(!data.save) cb(null,imgs); 
-		})
-	});
+  if(!data.size) setSize("medium");
+  if(data.size) setSize(data.size);
+  if(!data.search) return(cb("Please define what you want to search",null))
+  if(!cb) throw new Error("No callback defined")
+  getResponse(data.search,function(err,response){
+    if(!response) return(cb(err,null))
+    scrapImage(response.body,function(err,imgs){
+      if(!imgs) return(cb(err,null))
+      if(data.save){
+        saveImage({img:imgs[0],save:data.save},function(err,filename){
+          if(!filename) return(cb(err,null))
+          return(cb(null,filename))
+        })
+      }
+      if(!data.save) cb(null,imgs) 
+    })
+  })
+}
+
+function setSize(set){
+  if(options.api=="google"){
+    if(set == "small") options.size = "i"
+    if(set == "medium") options.size = "m"
+    if(set == "large") options.size = "l"
+  }
+  if(options.api=="pixplorer"){
+    if(set == "small") options.size = "s"
+    if(set == "medium") options.size = "m"
+    if(set == "large") options.size = "l"
+  }
 }
 
 function getResponse(search,cb){
-	request('http://www.google.com/search?q='+search+'&safe=off&source=lnms&tbm=isch&sa=X', function (err, response, body) {
-		if (err) return(cb(err,null))
-		if(response.statusCode != 200) return(cb("HTTP code is not giving a 200. Cannot continue, current HTTP code: "+response.statusCode),false);
-		cb(null, {response:response,body:body});
-	});
+  var url
+  if(options.api=="google") url = 'http://www.google.com/search?q='+search+'&safe=off&source=lnms&tbm=isch&sa=X&tbs=isz:'+options.size
+  if(options.api=="pixplorer") url = 'http://api.pixplorer.co.uk/image?word='+search+'&amount=20&size='+options.size
+  request(url, function (err, response, body) {
+    if (err) return(cb(err,null))
+    if(response.statusCode != 200) return(cb("HTTP code is not giving a 200. Cannot continue, current HTTP code: "+response.statusCode),null)
+    cb(null, {response:response,body:body})
+  })
 }
 
 function scrapImage(body,cb){
-	var $ = cheerio.load(body);
-	var table = $('table[class=images_table]').html();
-	if(!table) return(cb("Table class='images_table' could not be found in the scrapped results!",false));
-	var arrayImage = $('table[class=images_table] tr td a img');
-	var images = [];
-	for(i=0;i<arrayImage.length;i++){
-		images.push(arrayImage[i].attribs.src);
+  if(options.api=="google"){
+    var $ = cheerio.load(body)
+    var table = $('table[class=images_table]').html()
+    if(!table) return(cb("Table class='images_table' could not be found in the scrapped results!",null))
+    var arrayImage = $('table[class=images_table] tr td a img')
+    var images = []
+    for(i=0;i<arrayImage.length;i++){
+      images.push(arrayImage[i].attribs.src)
+    }
+    cb(null,images)
+  }
+  
+  if(options.api=="pixplorer"){
+    try {
+      var body = JSON.parse(body)
+    } catch (e) {
+			return(cb(e,null))
+		}
+		var images = []
+		for(var i=0;i<body.images.length;i++){
+			images.push(body.images[i].imageurl)
+		}
+		cb(null,images)
 	}
-	cb(null,images);
 }
 
 function saveImage(data,cb){
-	var filename = data.img.split("/")[data.img.split("/").length-1].split("images?q=tbn:")[1] + ".png";
-	request.get(data.img).on('error', function(err) {
-		return(cb(err,null));
+	var filename = sha256(data.img) + path.extname(data.img.split("%")[0].split("?")[0]);
+	var stream = request.get(data.img).on('error', function(err) {
+    return(cb(err,null))
 	}).pipe(fs.createWriteStream(path.join(data.save,filename)))
-	cb(null,filename);
-}
+  stream.on('finish', function () {
+    cb(null,filename)
+  })
+  stream.on('error', function (err) {
+    cb(err,null)
+  })
+} 
--- test/test.js
+++ test/test.js
@@ -2,25 +2,90 @@ var assert = require('assert');
 var assert = require('assert-plus');
 var fs = require('fs');
 var path = require('path');
-var googleImageSearch = require('../app');
+var scrapyImage = require('../app');
 
-describe("API", function(){ 
-	this.timeout(5500);
+describe("GOOGLE API", function(){ 
+	this.timeout(15000);
+	before(function() {
+		scrapyImage.scraper({api:"google"});
+	});
 	it(".search", function(done){
-		googleImageSearch.search({search:"test"},function(err,data){
+		scrapyImage.search({search:"test"},function(err,data){
 			assert.equal(err,null);
 			done();
 		});
 	});
 	it(".search and save to file", function(done){
-		googleImageSearch.search({search:"test",save:"C:/Users/CatsPC/Desktop/NPM/google-image-search/test"},function(err,data){
+		scrapyImage.search({search:"cat",save:"./test"},function(err,data){
+			fs.unlinkSync("./test/"+data);
 			assert.equal(err,null);
 			done();
 		});
 	});
 	it(".search (without a callback)", function(){
 		assert.throws(function(){
-			googleImageSearch.search({search:"test"});
+			scrapyImage.search({search:"lol"});
 		},Error);
+	});
+	it(".search large image", function(done){
+		scrapyImage.search({search:"test",size:"large"},function(err,data){
+			assert.equal(err,null);
+			done();
+		});
+	});
+	it(".search medium image", function(done){
+		scrapyImage.search({search:"test",size:"medium"},function(err,data){
+			assert.equal(err,null);
+			done();
+		});
+	});
+	it(".search small image", function(done){
+		scrapyImage.search({search:"test",size:"small"},function(err,data){
+			assert.equal(err,null);
+			done();
+		});
+	});
+});
+
+describe("PIXPLORER API", function(){ 
+	this.timeout(15000);
+	before(function() {
+		scrapyImage.scraper({api:"pixplorer"});
+	});
+	it(".search", function(done){
+		scrapyImage.search({search:"test"},function(err,data){
+			assert.equal(err,null);
+			done();
+		});
+	});
+	it(".search and save to file", function(done){
+		scrapyImage.search({search:"dog",save:"./test"},function(err,data){
+			fs.unlinkSync("./test/"+data);
+			assert.equal(err,null);
+			done();
+		});
+	});
+	it(".search (without a callback)", function(){
+		assert.throws(function(){
+			scrapyImage.search({search:"lol"});
+		},Error);
+	});
+	it(".search large image", function(done){
+		scrapyImage.search({search:"test",size:"large"},function(err,data){
+			assert.equal(err,null);
+			done();
+		});
+	});
+	it(".search medium image", function(done){
+		scrapyImage.search({search:"test",size:"medium"},function(err,data){
+			assert.equal(err,null);
+			done();
+		});
+	});
+	it(".search small image", function(done){
+		scrapyImage.search({search:"test",size:"small"},function(err,data){
+			assert.equal(err,null);
+			done();
+		});
 	});
 });
